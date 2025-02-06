c275b00dec62b1da0a3d027852f33aa51c0d4e5e
fabianRivero <fabianarj467@gmail.com>
2025-02-06 18:18:51 -0400

    ultimo commit

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ import usersRoutes from './routes/usersRoutes.js';
 import dotenv from 'dotenv';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
