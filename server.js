7dc15c9edbf8b9364a37bf7b8f5dc9e12abe7e19
fabianRivero <fabianarj467@gmail.com>
2025-02-06 16:16:38 -0400

    api lista

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import usersRoutes from './routes/usersRoutes.js';
 import dotenv from 'dotenv';
 
 const app = express();
-const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(cors());
@@ -46,10 +45,13 @@ app.get('/', (req, res) =>{
     res.status(200).send('hello');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 
 app.use('/api/urls', urlRoutes);
 app.use('/api/users', usersRoutes);
-
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
